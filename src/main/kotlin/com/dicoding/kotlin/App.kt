534ad25713fda6badb0048d60a7a2e228761471c
Rochmad Sigit <rochmad.sigit@gmail.com>
2020-06-18 15:33:41 +0700

    Initial Commit: Basic Structure and simple classes.

---
+++ src/main/kotlin/com/dicoding/kotlin/App.kt
@@ -0,0 +1,14 @@
+package com.dicoding.kotlin
+
+fun main(){
+    val text = "Hello Kotlin"
+    val result = text.run {
+        val from = this
+        val to = this.replace("Kotlin","Putri Chandra")
+        "replace text from $from to $to"
+    }
+    println("result: $result")
+
+
+
+}
